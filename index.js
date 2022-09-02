95782dbc72451d8313fe3dadb96d175b636116af
JayavarthiniR <jayavarthini.r@moonraft.com>
2022-09-02 08:58:03 +0530

    jaya

---
+++ index.js
@@ -0,0 +1,34 @@
+const hamburger=document.querySelector(".hamburger");
+const linkOptions=document.querySelector(".linkOptions");
+hamburger.addEventListener("click", () =>{
+      hamburger.classList.toggle("active");
+      linkOptions.classList.toggle("active");
+    })
+document.querySelectorAll(".linknumber").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    linkOptions.classList.remove("active");
+}))
+function submitit(){
+     var email = document.getElementById("email").value;
+     var validemail = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+    if(email.match(validemail))
+    {
+        document.getElementById("subscribeit").innerHTML = " Valid E-mailId!";
+    }
+    else
+    {
+        document.getElementById("subscribeit").innerHTML = " Invalid E-mailId!";
+    }
+}
+function verifySubmit(){
+    var email1 = document.getElementById("email1").value;
+    var validemail1 = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+   if(email1.match(validemail1))
+   {
+       document.getElementById("verifySubscribe").innerHTML = " Valid E-mailId!";
+   }
+   else
+   {
+       document.getElementById("verifySubscribe").innerHTML = " Invalid E-mailId!";
+   }
+}
