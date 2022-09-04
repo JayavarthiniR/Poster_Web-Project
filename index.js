d4b8182e727d4a0d9104c0299c09701527270fcd
JayavarthiniR <jayavarthini.r@moonraft.com>
2022-09-04 12:53:15 +0530

    asd

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function submitit(){
     }
     else
     {
-        document.getElementById("subscribeit").innerHTML = " Invalid E-mailId!";
+        alert("Invalid E-mailId!");
     }
 }
 function verifySubmit(){
@@ -29,6 +29,64 @@ function verifySubmit(){
    }
    else
    {
-       document.getElementById("verifySubscribe").innerHTML = " Invalid E-mailId!";
+    alert("Invalid E-mailId!");
    }
 }
+let aspirescontainer=[
+    {
+    cardimage:"Pictures/Image_placeholder.svg",
+    date:"01 MAY 2020",
+    heading:"Heading",
+    telescope:"Moment in the life of any aspiring astronomer of that it is time to buy that first telescope",
+    viewicon:"Pictures/visibilityIcon.svg",
+    viewcontent:"7.4K",
+    messageicon:"Pictures/messageIcon.svg",
+    messagecontent:"31",
+    },
+    {
+    cardimage:"Pictures/Image_placeholder.svg",
+    date:"01 MAY 2020",
+    heading:"Two line header example",
+    telescope:"Moment in the life of any aspiring astronomer of that it is time to buy that first telescope",
+    viewicon:"Pictures/visibilityIcon.svg",
+    viewcontent:"7.4K",
+    messageicon:"Pictures/messageIcon.svg",
+    messagecontent:"31",
+    },
+    {
+    cardimage:"Pictures/Image_placeholder.svg",
+    date:"01 MAY 2020",
+    heading:"Heading",
+    telescope:"Moment in the life of any aspiring astronomer of that it is time to buy that first telescope",
+    viewicon:"Pictures/visibilityIcon.svg",
+    viewcontent:"7.4K",
+    messageicon:"Pictures/messageIcon.svg",
+    messagecontent:"31",
+    },
+    {
+    cardimage:"Pictures/Image_placeholder.svg",
+    date:"01 MAY 2020",
+    heading:"Two line header example",
+    telescope:"Moment in the life of any aspiring astronomer of that it is time to buy that first telescope",
+    viewicon:"Pictures/visibilityIcon.svg",
+    viewcontent:"7.4K",
+    messageicon:"Pictures/messageIcon.svg",
+    messagecontent:"31",
+    }];
+    var card="";
+    for(var i=0;i<aspirescontainer.length;i++){
+        card+='<div class="telescope">';
+        card+="<div class='telescopeimg'><img src=" +aspirescontainer[i].cardimage+ '></div>';
+        card+='<p id="date">'+aspirescontainer[i].date+'</p>';
+        card+='<h3>'+aspirescontainer[i].heading+'</h2>';
+        card+='<p class="firstTelescope">'+aspirescontainer[i].telescope+'</p>' ;
+        card+='<div class="viewsmessages">';
+        card+='<div class="views">';
+        card+="<div><img src=" +aspirescontainer[i].viewicon+ '></div>';
+        card+='<div>'+aspirescontainer[i].viewcontent+ '</div> </div>';
+        card+='<div class="messages">';
+        card+="<div><img src=" +aspirescontainer[i].messageicon+ '></div>';
+        card+='<div>'+aspirescontainer[i].messagecontent+ '</div> </div> </div> </div>';
+        
+    }
+    document.getElementById("aspires_content").innerHTML=card;
